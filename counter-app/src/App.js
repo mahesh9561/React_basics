17aa36fa7d89adf63809cadda45791abd6c7cf24
mahesh9561 <maheshpathak200@gmail.com>
2023-09-11 14:40:15 +0530

    BG-COLOR Changer

---
+++ counter-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+// import Card from './components/Card';
+import Bgcolor from './components/bgcolor';
+function App() {
+
+
+  return (
+    <div className="App">
+      {/* <Card username = "Mahesh" btnclick = "Click me" />
+      <Card username = "Vivek" btnclick = "View me" /> */}
+
+      <Bgcolor/>
+    </div>
+  );
+}
+
+export default App;
